c0c109419db9f6c5bf47f67c0c963828bfc5bb7c
vann <vann@innerfusion.net>
2010-10-20 14:48:24 -0500

    add bam_helpers module

---
+++ lib/bam_helpers.rb
@@ -0,0 +1,14 @@
+module BamHelpers
+  # terminal column width
+  def col_width
+    `stty size`.split(" ").last.to_i
+  end
+  
+  def border
+    "="*col_width
+  end
+  
+  def wrap_borders(content)
+    "#{border}\n#{content}\n#{border}"
+  end
+end
