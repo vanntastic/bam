bc847b7dac4011f854748f4e90a596fe537d0e90
vann <vann@innerfusion.net>
2010-10-20 14:48:11 -0500

    bump to version 0.0.4, update readme, refactored helper methods, update bamify command

--- lib/bam.rb
+++ lib/bam.rb
@@ -1,5 +1,6 @@
 module Bam
   class Deployment
+    include BamHelpers
     PWD = Dir.pwd
 
     def initialize(server, to, from)
@@ -24,28 +25,19 @@ module Bam
       exclude_list = exclude_list.join
     end
 
-    # terminal column width
-    def col_width
-      `stty size`.split(" ").last.to_i
-    end
-    
-    def border
-      "="*col_width
-    end
-
     def deploy
       puts("Starting deployment...")
       # use -avzC to exclude .git and .svn repositories
       cmd = "rsync -avzC #{@from} #{@server}:#{@to} #{exclusions}"
       output = "OUTPUT: #{cmd}"
-      puts("#{border}\n#{output}\n#{border}")
+      puts(wrap_borders(output))
       system(cmd)
     end
     
     def deploy_tasks(tasks)
       if tasks.length > 0
         tasks.each do |task|
-          puts "- Executing deployment task: #{task}"
+          puts "  - Executing deployment task: #{task}"
           `#{task}`
         end
       end
