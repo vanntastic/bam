95294b068c478ef7acc4c5624682cab0d91eafd2
Vann Ek <ek.vann@gmail.com>
2015-11-18 16:09:32 -0600

    modify dry_run behavior, cleanup formatting

--- lib/bam.rb
+++ lib/bam.rb
@@ -3,10 +3,11 @@ module Bam
     include BamHelpers
     PWD = Dir.pwd
 
-    def initialize(server, to, from)
+    def initialize(server, to, from, dry_run)
       @from = from
       @server = server
       @to = to
+      @dry_run = dry_run || false
     end
 
     def has_git?
@@ -17,30 +18,36 @@ module Bam
       File.exists?(File.join PWD, ".gitignore")
     end
 
-    def exclusions
-      return "" if !has_git_ignores? || !has_git?
+    # gets a list of exclusions - exceptions
+    def get_exclusions(exceptions)
+      return "" if !has_git_ignores? || !has_git?   
       git_ignore = File.join PWD, ".gitignore"
-      exclusions = `cat #{git_ignore}`.split("\n")
-      exclude_list = exclusions.map { |e| "--exclude '#{e}' " }
+      exclusions = `cat #{git_ignore}`.split("\n") 
+      exclusions - exceptions
+    end
+
+    def exclusions(exceptions)
+      exclude_list = get_exclusions(exceptions).map { |e| "--exclude '#{e}' " }
       exclude_list = exclude_list.join
     end
 
-    def deploy
-      puts("Starting deployment...")
+    def deploy(exceptions)
+      puts(wrap_top("STARTING DEPLOYMENT:"))
       # use -avzC to exclude .git and .svn repositories
-      cmd = "rsync -avzC #{@from} #{@server}:#{@to} #{exclusions}"
+      cmd = "rsync -avzC #{@from} #{@server}:#{@to} #{exclusions(exceptions)}"
       output = "OUTPUT: #{cmd}"
       puts(wrap_borders(output))
-      system(cmd)
+      system(cmd) unless @dry_run
     end
     
     def deploy_tasks(tasks)
       if tasks.length > 0
         tasks.each do |task|
-          puts "  - Executing deployment task: #{task}"
-          `#{task}`
+          puts "[TASK] #{task}"
+          `#{task}` unless @dry_run
         end
       end
+      puts "\n"
     end
     
   end
