2162b61990955d38a6b2dfa15a4ddaf0039d0534
Vann Ek <ek.vann@gmail.com>
2015-11-18 22:06:09 -0600

    change deploy.bam.rb to deploy.bam, use optparse for option parsing, add remote tasks, add pre and post deployment tasks to core bam.rb

--- lib/bam.rb
+++ lib/bam.rb
@@ -3,11 +3,12 @@ module Bam
     include BamHelpers
     PWD = Dir.pwd
 
-    def initialize(server, to, from, dry_run)
+    def initialize(server, to, from, dry_run, task_config)
       @from = from
       @server = server
       @to = to
       @dry_run = dry_run || false
+      @task_config = task_config
     end
 
     def has_git?
@@ -19,32 +20,52 @@ module Bam
     end
 
     # gets a list of exclusions - exceptions
-    def get_exclusions(exceptions)
+    def get_exclusions
       return "" if !has_git_ignores? || !has_git?   
       git_ignore = File.join PWD, ".gitignore"
       exclusions = `cat #{git_ignore}`.split("\n") 
-      exclusions - exceptions
+      exclusions - @task_config[:always_include] 
     end
 
-    def exclusions(exceptions)
-      exclude_list = get_exclusions(exceptions).map { |e| "--exclude '#{e}' " }
+    def exclusions
+      exclude_list = get_exclusions.map { |e| "--exclude '#{e}' " }
       exclude_list = exclude_list.join
     end
 
-    def deploy(exceptions)
+    def remote_exec(name)
+      remote_task = @task_config[:remote][name.to_sym]
+      if remote_task.nil?
+        puts "[REMOTE] TASK: #{name} does not exist"
+      else
+        exec_cmd = "ssh #{@server} '#{remote_task}'"
+        puts @dry_run ? "[REMOTE] #{name} : #{remote_task}" : "[REMOTE] #{name}"
+        system exec_cmd unless @dry_run
+      end
+    end
+
+    def deploy
+      puts "PRE-DEPLOYMENT TASKS: \n\n"
+      deploy_tasks @task_config[:pre]
       puts(wrap_top("STARTING DEPLOYMENT:"))
       # use -avzC to exclude .git and .svn repositories
-      cmd = "rsync -avzC #{@from} #{@server}:#{@to} #{exclusions(exceptions)}"
+      cmd = "rsync -avzC #{@from} #{@server}:#{@to} #{exclusions}"
       output = "OUTPUT: #{cmd}"
       puts(wrap_borders(output))
+      puts "POST-DEPLOYMENT TASKS: \n\n"
+      deploy_tasks @task_config[:post]
       system(cmd) unless @dry_run
     end
     
     def deploy_tasks(tasks)
       if tasks.length > 0
         tasks.each do |task|
-          puts "[TASK] #{task}"
-          `#{task}` unless @dry_run
+          remote_task = task.split("remote:")
+          if remote_task.length > 1
+            remote_exec remote_task[1]
+          else 
+            puts "[LOCAL] #{task}"
+            `#{task}` unless @dry_run
+          end
         end
       end
       puts "\n"
